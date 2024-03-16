b80bad1a29fee342bf44e6b38bccd2736ef8d99b
Ezra Huang <ezrahuang155@gmail.com>
2024-03-16 14:55:46 -0400

    single machine actor implementation

--- replay_buffers/prioritized_replay_buffer.py
+++ replay_buffers/prioritized_replay_buffer.py
@@ -49,6 +49,19 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         # print("Storing in PrioritizedReplayBuffer Time ", time() - time1)
         return transition
 
+    def store_with_priority(
+        self, observation, action, reward, next_observation, done, priority
+    ):
+        transition = super().store(observation, action, reward, next_observation, done)
+
+        if transition:
+            self.sum_tree[self.tree_pointer] = priority**self.alpha
+            self.min_tree[self.tree_pointer] = priority**self.alpha
+            self.max_priority = max(self.max_priority, priority)
+            self.tree_pointer = (self.tree_pointer + 1) % self.max_size
+
+        return transition
+
     def sample(self, beta=0.4):
         # print("Sampling from PrioritizedReplayBuffer")
         # time1 = 0
