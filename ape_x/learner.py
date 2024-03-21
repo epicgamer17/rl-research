be93b0eb553ab471ccaa645332a37050ee3c7a43
Ezra Huang <ezrahuang155@gmail.com>
2024-03-20 23:33:40 -0400

    learner refactor

--- ape_x/learner.py
+++ ape_x/learner.py
@@ -3,6 +3,8 @@ import logging
 import time
 import tensorflow as tf
 import numpy as np
+import queue
+import threading
 
 sys.path.append("../")
 from rainbow.rainbow_agent import RainbowAgent
@@ -15,20 +17,35 @@ matplotlib.use("Agg")
 logger = logging.getLogger(__name__)
 
 
+# signals are sent into queues
+# queues are constantly read by the learner rpc thread
+
+
 class LearnerBase(RainbowAgent):
     def __init__(self, env, config):
         super().__init__(model_name="learner", env=env, config=config)
         self.graph_interval = 200
         self.remove_old_experiences_interval = config["remove_old_experiences_interval"]
-        self.running = False
 
-    def sample_experiences_from_remote_replay_buffer(self):
+        self.push_weights_interval = 1
+        self.weights_queue = queue.Queue(16)
+        self.priority_updates_queue = queue.Queue(
+            16
+        )  # contains tuples of (ids, indices, priorities)
+
+        self.samples_queue_size = 64
+        self.samples_queue = queue.Queue(
+            self.samples_queue_size
+        )  # contains tuples of (sample, n_step_sample)
+        self.finished_event = threading.Event()
+
+    def consume_weights(self):
         pass
 
-    def sample_n_step_experiences_from_remote_replay_buffer(self, indices):
+    def produce_samples(self):
         pass
 
-    def update_remote_replay_buffer_priorities(self, indices, priorities):
+    def consume_priority_updates(self):
         pass
 
     def remove_old_experiences_from_remote_replay_buffer(self):
@@ -37,13 +54,16 @@ class LearnerBase(RainbowAgent):
     def _experience_replay(self):
         with tf.GradientTape() as tape:
             elementwise_loss = 0
-            samples = self.sample_experiences_from_remote_replay_buffer()
+            samples, n_step_samples = self.samples_queue.get()
+
+            ids = samples["ids"]
+            indices = samples["indices"]
             actions = samples["actions"]
             observations = samples["observations"]
             inputs = self.prepare_states(observations)
             weights = samples["weights"].reshape(-1, 1)
-            indices = samples["indices"]
             discount_factor = self.discount_factor
+
             target_ditributions = self.compute_target_distributions(
                 samples, discount_factor
             )
@@ -62,9 +82,6 @@ class LearnerBase(RainbowAgent):
 
             # if self.use_n_step:
             discount_factor = self.discount_factor**self.n_step
-            n_step_samples = self.sample_n_step_experiences_from_remote_replay_buffer(
-                indices
-            )
             actions = n_step_samples["actions"]
             n_step_observations = n_step_samples["observations"]
             observations = n_step_observations
@@ -104,13 +121,31 @@ class LearnerBase(RainbowAgent):
         prioritized_loss = np.clip(
             prioritized_loss, 0.01, tf.reduce_max(prioritized_loss)
         )
-        self.update_remote_replay_buffer_priorities(indices, prioritized_loss)
+
+        try:
+            logger.debug("pushing update")
+            self.priority_updates_queue.put(
+                (ids, indices, prioritized_loss), block=False
+            )
+        except queue.Full:
+            logger.warning(f"priority_updates_queue full, dropping priority update")
+
         self.model.reset_noise()
         self.target_model.reset_noise()
         loss = loss.numpy()
         return loss
 
-    def run(self, graph_interval=200):
+    def run(self, graph_interval=2):
+        consume_weights_thread = threading.Thread(target=self.consume_weights)
+        produce_samples_thread = threading.Thread(target=self.produce_samples)
+        consume_priority_updates_thread = threading.Thread(
+            target=self.consume_priority_updates
+        )
+
+        consume_weights_thread.start()
+        produce_samples_thread.start()
+        consume_priority_updates_thread.start()
+
         logger.info("learner running")
         self.is_test = False
         stat_score = (
@@ -119,58 +154,108 @@ class LearnerBase(RainbowAgent):
         stat_test_score = []
         stat_loss = []
         # self.fill_replay_buffer()
-        num_trials_truncated = 0
         state, _ = self.env.reset()
         model_update_count = 0
         score = 0
         training_step = 0
-        while training_step < self.num_training_steps:
+
+        # push initial weights
+        self.weights_queue.put(self.model.get_weights())
+
+        while training_step <= self.num_training_steps:
             logger.info(
                 f"learner training step: {training_step}/{self.num_training_steps}"
             )
             self.per_beta = min(1.0, self.per_beta + self.per_beta_increase)
 
-            if self.replay_buffer.size >= self.replay_batch_size:
-                model_update_count += 1
-                loss = self._experience_replay()
-                training_step += 1
-                stat_loss.append(loss)
-                self.update_target_model(model_update_count)
-            else:
-                time.sleep(0.1)
+            model_update_count += 1
+            loss = self._experience_replay()
+            training_step += 1
+            stat_loss.append(loss)
+            self.update_target_model(model_update_count)
 
             if training_step % graph_interval == 0 and training_step > 0:
-                self.save_checkpoint(training_step)
+                # self.save_checkpoint(training_step)
                 # stat_test_score.append(self.test())
                 self.plot_graph(stat_score, stat_loss, stat_test_score, training_step)
 
+            if training_step % self.push_weights_interval and training_step > 0 == 0:
+                try:
+                    logger.debug("pushing weights to remote")
+                    self.weights_queue.put(self.model.get_weights(), False)
+                except queue.Full:
+                    logger.warning(f"weights_queue full, dropping weight update")
+
+        logger.info("loop done")
+
+        self.priority_updates_queue.put(None)
+        self.weights_queue.put(None)
+
+        self.finished_event.set()
+
+        produce_samples_thread.join()
+        consume_weights_thread.join()
+        consume_priority_updates_thread.join()
+
         self.plot_graph(stat_score, stat_loss, stat_test_score, training_step)
-        self.save_checkpoint(training_step)
+        # self.save_checkpoint(training_step)
         self.env.close()
-        return num_trials_truncated / self.num_training_steps
 
 
 class SingleMachineLearner(LearnerBase):
     def __init__(self, env, config):
         super().__init__(env=env, config=config)
 
-    def sample_experiences_from_remote_replay_buffer(self):
-        print("Sampling experiences from remote replay buffer")
-        return self.replay_buffer.sample()
+    def consume_weights(self):
+        weights = self.weights_queue.get()
 
-    def sample_n_step_experiences_from_remote_replay_buffer(self, indices):
-        print("Sampling n-step experiences from remote replay buffer")
-        return self.n_step_replay_buffer.sample_from_indices(indices)
+        while weights is not None:
+            print("push weights to remote here")
+            weights = self.weights_queue.get()
 
-    def update_remote_replay_buffer_priorities(self, indices, priorities):
-        print("Updating remote replay buffer priorities")
-        return self.replay_buffer.update_priorities(indices, priorities)
+            # push sample
+
+        logger.info("consume weights finished")
+
+    def consume_priority_updates(self):
+        t = self.priority_updates_queue.get()
+
+        while t is not None:
+            logger.debug("updating priorities")
+            ids, indices, prioritized_loss = t
+            self.replay_buffer.update_priorities(indices, prioritized_loss, ids)
+            t = self.priority_updates_queue.get()
+
+        logger.info("consume priority finished")
+
+    def produce_samples(self):
+        target_size = self.samples_queue_size / 2
+
+        while not self.finished_event.isSet():
+            logger.debug("polling for flag")
+            if self.samples_queue.qsize() < target_size:
+                if self.replay_buffer.size < self.min_replay_buffer_size:
+                    logger.debug(
+                        "replay buffer not large enough to sample from, polling"
+                    )
+                    time.sleep(1)
+                    continue
+                logger.debug("sampling")
+                samples = self.replay_buffer.sample()
+                indicies = samples["indices"]
+                n_step_samples = self.n_step_replay_buffer.sample_from_indices(indicies)
+                self.samples_queue.put((samples, n_step_samples))
+            else:
+                time.sleep(1)
+
+        logger.info("produce samples finished")
 
     def remove_old_experiences_from_remote_replay_buffer(self):
         # done atomatically in the replay buffer (old experiences overwritten)
         pass
 
     def get_weights(self):
+        # helper for single machine
         return self.model.get_weights()
 
 
@@ -191,8 +276,8 @@ class DistributedLearner(LearnerBase):
         return self.replay_buffer.update_priorities(indices, priorities)
 
     def remove_old_experiences_from_remote_replay_buffer(self):
-        print("Removing old experiences from remote replay buffer")
-        return self.replay_buffer.remove_old_experiences()
+        # done automatically in the replay buffer (old experiences overwritten)
+        pass
 
     def get_weights(self):
         return self.model.get_weights()
