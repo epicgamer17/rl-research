bc300dbe78b3ca5ae4dba04c6bff2b620aaeedc8
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2024-03-25 15:03:29 -0400

    more config objects made

--- configs/base_config.py
+++ configs/base_config.py
@@ -3,36 +3,44 @@ import tensorflow as tf
 
 class Config:
     def __init__(self, config) -> None:
-
         # could take in a game config and set an action space and observation shape here
         # OR DO THAT IN BASE AGENT?
 
-        if config != {}:
-            self.optimizer = config["optimizer"]
-            self.adam_epsilon = config["adam_epsilon"]
-            self.learning_rate = config["learning_rate"]
-            self.clipnorm = config["clipnorm"]
+        # ADD LEARNING RATE SCHEDULES
 
-            self.replay_batch_size = int(config["replay_batch_size"])
-            self.replay_period = int(config["replay_period"])
-            self.replay_buffer_size = max(
-                int(config["replay_buffer_size"]), self.replay_batch_size
-            )
-            self.min_replay_buffer_size = int(config["min_replay_buffer_size"])
+        self.optimizer = (
+            config["optimizer"]
+            if "optimizer" in config
+            else tf.keras.optimizers.legacy.Adam
+        )
+        self.adam_epsilon = config["adam_epsilon"] if "adam_epsilon" in config else 1e-6
+        self.learning_rate = (
+            config["learning_rate"] if "learning_rate" in config else 0.01
+        )
+        self.clipnorm = config["clipnorm"] if "clipnorm" in config else None
 
-            self.num_training_steps = int(config["num_training_steps"])
-        else:
-            self._set_default_config()
+        self.loss_function = (
+            config["loss_function"] if "loss_function" in config else None
+        )
+        assert self.loss_function is not None, "Loss function must be defined"
 
-    def _set_default_config(self):
-        self.optimizer = tf.keras.optimizers.legacy.Adam
-        self.adam_epsilon = 1e-6
-        self.learning_rate = 0.01
-        self.clipnorm = None
+        self.replay_batch_size = (
+            int(config["replay_batch_size"]) if "replay_batch_size" in config else 32
+        )
+        self.replay_period = (
+            int(config["replay_period"]) if "replay_period" in config else 1
+        )
+        self.replay_buffer_size = (
+            max(int(config["replay_buffer_size"]), self.replay_batch_size)
+            if "replay_buffer_size" in config
+            else 1024
+        )
+        self.min_replay_buffer_size = (
+            int(config["min_replay_buffer_size"])
+            if "min_replay_buffer_size" in config
+            else 0
+        )
 
-        self.replay_batch_size = 32
-        self.replay_period = 1
-        self.replay_buffer_size = 1024
-        self.min_replay_buffer_size = 0
-
-        self.num_training_steps = 100
+        self.training_steps = (
+            int(config["training_steps"]) if "training_steps" in config else 100
+        )
--- configs/cartpole_config.py
+++ configs/cartpole_config.py
@@ -1,4 +1,7 @@
-class CartPoleConfig:
+from game_config import GameConfig
+
+
+class CartPoleConfig(GameConfig):
     def __init__(self):
         self.max_score = 500
         self.min_score = 0
