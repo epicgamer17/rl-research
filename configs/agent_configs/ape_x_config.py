5f850046fe524f51f8033fda1769187bd802bf74
Ezra Huang <ezrahuang155@gmail.com>
2024-03-30 13:59:11 -0400

    reduce config code duplication

--- configs/agent_configs/ape_x_config.py
+++ configs/agent_configs/ape_x_config.py
@@ -1,5 +1,37 @@
+from configs.agent_configs.base_config import ConfigBase
 from configs.agent_configs.rainbow_config import RainbowConfig
 import tensorflow as tf
+
+
+class DistributedConfig(ConfigBase):
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
+
+        self.learner_addr: str = self.parse_field("learner_addr", required=True)
+        self.learner_port: int = self.parse_field("learner_port", required=True)
+        self.replay_addr: str = self.parse_field("replay_addr", required=True)
+        self.replay_port: int = self.parse_field("replay_port", required=True)
+
+
+class LearnerApeXMixin(ConfigBase):
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
+
+        self.samples_queue_size: int = self.parse_field("samples_queue_size", 16)
+        self.updates_queue_size: int = self.parse_field("updates_queue_size", 16)
+        self.remove_old_experiences_interval: int = self.parse_field(
+            "remove_old_experiences_interval", 1000
+        )
+
+        # not used
+        self.push_params_interval: int = self.parse_field("push_params_interval", 100)
+
+
+class ActorApeXMixin(ConfigBase):
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
+
+        self.poll_params_interval: int = self.parse_field("poll_params_interval", 100)
 
 
 class ApeXConfig(RainbowConfig):
@@ -9,70 +41,3 @@ class ApeXConfig(RainbowConfig):
         # CHANGE CONFIGS TO WORK FOR APE-X AGENT
         # SHOULD BE ONE CONFIG BUT CAN HAVE CHILD CONFIGS (LOOK AT PPO)
         # so could just be like num actors etc <- since i think the others can just use the rainbow parameters from this config
-        if "poll_params_interval" in config_dict:
-            self.poll_params_interval = config_dict["poll_params_interval"]
-        else:
-            self.poll_params_interval = 100
-            print("Using default poll params interval: 100")
-
-        if "buffer_size" in config_dict:
-            self.buffer_size = config_dict["buffer_size"]
-        else:
-            self.buffer_size = 100
-            print("Using default buffer size: 100")
-
-        if "learner_addr" in config_dict:
-            self.learner_addr = config_dict["learner_addr"]
-        else:
-            self.learner_addr = None
-            assert self.learner_addr is not None, "Learner address must be set"
-
-        if "learner_port" in config_dict:
-            self.learner_port = config_dict["learner_port"]
-        else:
-            self.learner_port = None
-            assert self.learner_port is not None, "Learner port must be set"
-
-        if "replay_addr" in config_dict:
-            self.replay_addr = config_dict["replay_addr"]
-        else:
-            self.replay_addr = None
-            assert self.replay_addr is not None, "Replay address must be set"
-
-        if "replay_port" in config_dict:
-            self.replay_port = config_dict["replay_port"]
-        else:
-            self.replay_port = None
-            assert self.replay_port is not None, "Replay port must be set"
-
-        if "remove_old_experiences_interval" in config_dict:
-            self.remove_old_experiences_interval = config_dict[
-                "remove_old_experiences_interval"
-            ]
-        else:
-            self.remove_old_experiences_interval = 1000
-            print("Using default remove old experiences interval: 1000")
-
-        if "push_weights_interval" in config_dict:
-            self.push_weights_interval = config_dict["push_weights_interval"]
-        else:
-            self.push_weights_interval = 20
-            print("Using default push weights interval: 20")
-
-        if "samples_queue_size" in config_dict:
-            self.samples_queue_size = config_dict["samples_queue_size"]
-        else:
-            self.samples_queue_size = 16
-            print("Using default samples queue size: 16")
-
-        if "updates_queue_size" in config_dict:
-            self.updates_queue_size = config_dict["updates_queue_size"]
-        else:
-            self.updates_queue_size = 16
-            print("Using default updates queue size: 16")
-
-        if "port" in config_dict:
-            self.port = config_dict["port"]
-        else:
-            self.port = None
-            assert self.port is not None, "Port must be set"
--- configs/agent_configs/rainbow_config.py
+++ configs/agent_configs/rainbow_config.py
@@ -7,116 +7,31 @@ class RainbowConfig(Config):
         super(RainbowConfig, self).__init__(config_dict, game_config)
 
         # Network Arcitecture
-        if "width" in config_dict:
-            self.width = config_dict["width"]
-        else:
-            self.width = 128
-            print("Using default width: 128")
 
-        if "noisy_sigma" in config_dict:
-            self.noisy_sigma = config_dict["noisy_sigma"]
-        else:
-            self.noisy_sigma = 0.5
-            print("Using default noisy sigma: 0.5")
+        self.width: int = self.parse_field("width", 128)
+        self.noisy_sigma: float = self.parse_field("noisy_sigma", 0.5)
+        self.conv_layers: list | None = self.parse_field("conv_layers", required=False)
+        self.conv_layers_noisy: bool = self.parse_field("conv_layers_noisy", False)
+        self.dense_layers: int = self.parse_field("dense_layers", 1)
+        self.dense_layers_noisy: bool = self.parse_field("dense_layers_noisy", False)
+        self.value_hidden_layers: int = self.parse_field("value_hidden_layers", 0)
+        self.advantage_hidden_layers: int = self.parse_field(
+            "advantage_hidden_layers", 0
+        )
+        self.discount_factor: float = self.parse_field("discount_factor", 0.99)
+        self.soft_update: bool = self.parse_field("soft_update", False)
+        self.transfer_interval: int = self.parse_field("transfer_interval", 512)
+        self.ema_beta: float = self.parse_field("ema_beta", 0.99)
+        self.replay_interval: int = self.parse_field("replay_interval", 4)
+        self.per_alpha: float = self.parse_field("per_alpha", 0.6)
+        self.per_beta: float = self.parse_field("per_beta", 0.5)
+        self.per_epsilon: float = self.parse_field("per_epsilon", 1e-6)
+        self.n_step: int = self.parse_field("n_step", 3)
+        self.atom_size: int = self.parse_field("atom_size", 51)
 
-        if "conv_layers" in config_dict:
-            self.conv_layers = config_dict["conv_layers"]
-        else:
-            self.conv_layers = None
-            print("No convolutional layers set")
-            assert not (
-                self.game.is_image and self.conv_layers is not None
-            ), "Convolutional layers must be defined for image based games"
-
-        if "conv_layers_noisy" in config_dict:
-            self.conv_layers_noisy = config_dict["conv_layers_noisy"]
-        else:
-            self.conv_layers_noisy = False
-            print("No convolutional layers noisy set")
-
-        if "dense_layers" in config_dict:
-            self.dense_layers = config_dict["dense_layers"]
-        else:
-            self.dense_layers = 1
-            print("Using default number of dense layers: 1")
-
-        if "dense_layers_noisy" in config_dict:
-            self.dense_layers_noisy = config_dict["dense_layers_noisy"]
-        else:
-            self.dense_layers_noisy = False
-            print("No dense layers noisy set")
-
-        if "value_hidden_layers" in config_dict:
-            self.value_hidden_layers = config_dict["value_hidden_layers"]
-        else:
-            self.value_hidden_layers = 0
-            print("Using default number of value hidden layers: 0")
-
-        if "advantage_hidden_layers" in config_dict:
-            self.advantage_hidden_layers = config_dict["advantage_hidden_layers"]
-        else:
-            self.advantage_hidden_layers = 0
-            print("Using default number of advantage hidden layers: 0")
-
-        if "discount_factor" in config_dict:
-            self.discount_factor = config_dict["discount_factor"]
-        else:
-            self.discount_factor = 0.99
-            print("Using default discount factor: 0.99")
-
-        if "soft_update" in config_dict:
-            self.soft_update = config_dict["soft_update"]
-        else:
-            self.soft_update = False
-            print("No soft update set")
-
-        if "transfer_interval" in config_dict:
-            self.transfer_interval = int(config_dict["transfer_interval"])
-        else:
-            self.transfer_interval = 512
-            print("Using default transfer interval: 512")
-
-        if "ema_beta" in config_dict:
-            self.ema_beta = config_dict["ema_beta"]
-        else:
-            self.ema_beta = 0.99
-            print("Using default ema beta: 0.99")
-
-        if "replay_interval" in config_dict:
-            self.replay_interval = int(config_dict["replay_interval"])
-        else:
-            self.replay_interval = 4
-            print("Using default replay interval: 4")
-
-        if "per_alpha" in config_dict:
-            self.per_alpha = config_dict["per_alpha"]
-        else:
-            self.per_alpha = 0.6
-            print("Using default per alpha: 0.6")
-
-        if "per_beta" in config_dict:
-            self.per_beta = config_dict["per_beta"]
-        else:
-            self.per_beta = 0.5
-            print("Using default per beta: 0.5")
-
-        if "per_epsilon" in config_dict:
-            self.per_epsilon = config_dict["per_epsilon"]
-        else:
-            self.per_epsilon = 1e-6
-            print("Using default per epsilon: 1e-6")
-
-        if "n_step" in config_dict:
-            self.n_step = config_dict["n_step"]
-        else:
-            self.n_step = 3
-            print("Using default n step: 3")
-
-        if "atom_size" in config_dict:
-            self.atom_size = config_dict["atom_size"]
-        else:
-            self.atom_size = 51
-            print("Using default atom size: 51")
+        assert not (
+            self.game.is_image and self.conv_layers is not None
+        ), "Convolutional layers must be defined for image based games"
 
         # maybe don't use a game config, since if tuning for multiple games this should be the same regardless of the game <- (it is really a hyper parameter if you are tuning for multiple games or a game with unknown bounds)
 
