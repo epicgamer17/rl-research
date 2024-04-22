0fcebf18435589c61f6700407279b42b3de35927
Ezra Huang <ezrahuang155@gmail.com>
2024-04-21 23:32:31 -0400

    clean up distributed config handling within go

---
+++ ape_x/go/internal/pkg/configs/main.go
@@ -0,0 +1,18 @@
+package configs
+
+type DistributedConfig struct {
+	ReplayHost            string
+	ReplayLearnerPort     int
+	ReplayActorsPort      int
+	MongoHost             string
+	MongoPort             int
+	MongoUsername         string
+	MongoPasswordLocation string
+	SpectatorHost         string
+	ActorHosts            []string
+	LearnerHost           string
+	LearnerConfigFilename string
+	ActorConfigFilename   string
+	ReplayConfigFilename  string
+	WithLearner           bool
+}
