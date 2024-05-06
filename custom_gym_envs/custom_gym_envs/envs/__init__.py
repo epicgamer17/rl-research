d7c74a9fff25ea00e632cf9628a1e64bcfb3c07f
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2024-05-06 03:44:18 -0400

    made mississippi marbles env
    its a simple gambling game that is non perfect information. good for testing nfsp

--- custom_gym_envs/custom_gym_envs/envs/__init__.py
+++ custom_gym_envs/custom_gym_envs/envs/__init__.py
@@ -1,3 +1,4 @@
 from custom_gym_envs.envs.tictactoe import TicTacToeEnv
 from custom_gym_envs.envs.connect4 import Connect4Env
 from custom_gym_envs.envs.grid_world import GridWorldEnv
+from custom_gym_envs.envs.mississippi_marbles import MississippiMarblesEnv
