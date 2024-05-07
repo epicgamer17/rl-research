5ecfec5d528c02e9f9bc5dd1f7907989884b548c
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2024-05-07 03:33:40 -0400

    more bug fixes for NFSP

--- configs/agent_configs/agent_configs/nfsp_config.py
+++ configs/agent_configs/agent_configs/nfsp_config.py
@@ -1,18 +1,24 @@
 from agent_configs.sl_config import SupervisedConfig
 from .base_config import Config, ConfigBase
+from keras.optimizers import Optimizer, Adam
 
 
 class NFSPConfig(Config):
     def __init__(self, config_dict, game_config, rl_config_type):
         super(NFSPConfig, self).__init__(config_dict, game_config)
-        self.rl_config = rl_config_type(config_dict, game_config)
-        self.sl_config = SupervisedConfig(config_dict)
-        self.sl_config.game = game_config
+        self.num_players = self.parse_field("num_players", required=True)
+        self.rl_configs = [
+            rl_config_type(config_dict, game_config) for _ in range(self.num_players)
+        ]
+        self.sl_configs = [
+            SupervisedConfig(config_dict) for _ in range(self.num_players)
+        ]
+        for c in self.sl_configs:
+            c.game = game_config
 
         self.replay_interval = self.parse_field("replay_interval", 1)
 
         self.anticipatory_param = self.parse_field("anticipatory_param", 0.5)
-        self.num_players = self.parse_field("num_players", required=True)
 
     def _verify_game(self):
         assert self.game.is_discrete, "NFSP only supports discrete action spaces"
--- configs/game_configs/game_configs/__init__.py
+++ configs/game_configs/game_configs/__init__.py
@@ -4,3 +4,4 @@ from .tictactoe_config import TicTacToeConfig
 from .game_config import GameConfig
 from .cartpole_config import CartPoleConfig
 from .mississippi_marbles_config import MississippiMarblesConfig
+from .leduc_holdem_config import LeducHoldemConfig
