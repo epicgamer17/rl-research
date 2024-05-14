8efb6a9ca7447465a6072885ada42c2006317a12
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2024-05-14 12:08:19 -0400

    Updated armed bandits env

--- custom_gym_envs/custom_gym_envs/envs/armed_bandits.py
+++ custom_gym_envs/custom_gym_envs/envs/armed_bandits.py
@@ -10,14 +10,18 @@ class ArmedBanditsEnv(gym.Env):
     def __init__(
         self,
         render_mode=None,
-        means=[np.random.rand() for _ in range(1000)],
+        means=[np.random.uniform(-1, 1) for _ in range(1000)],
         std_devs=[np.random.rand() for _ in range(1000)],
+        steps=1000,
     ):
         assert len(means) == len(std_devs)
         self.means = means
         self.std_devs = std_devs
         self.observation_space = spaces.Discrete(1)  # or 0
         self.action_space = spaces.Discrete(len(means))
+
+        self.total_steps = steps
+        self.spec.reward_threshold = np.max(means) * self.total_steps
 
         assert render_mode is None
 
@@ -31,15 +35,17 @@ class ArmedBanditsEnv(gym.Env):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)  # might not set tfp seed
 
+        self.current_step = 0
         observation = self._get_obs()
         info = self._get_info()
         return observation, info
 
     def step(self, action):
+        self.current_step += 1
         reward = np.random.normal(self.means[action], self.std_devs[action])
         observation = self._get_obs()
         info = self._get_info()
-        return observation, reward, False, False, info
+        return observation, reward, False, self.current_step == self.total_steps, info
 
     def render(self):
         pass
