32c5076d4c9053a2dbaa9c4707f09f7a6614f274
Ezra Huang <ezrahuang155@gmail.com>
2024-05-18 19:14:11 -0400

    fix setup script and update setup instructions

--- ape_x/scripts/setup.sh
+++ ape_x/scripts/setup.sh
@@ -2,25 +2,6 @@
 
 # script to setup the environment for ape-x on open-gpu-x
 
-# install capnp from source
-install_capnp() {
-  # don't install if already installed
-
-  if [ -f "$HOME/.local/bin/capnp" ]; then
-    echo "capnp already installed"
-    return
-  fi
-
-  curl -O https://capnproto.org/capnproto-c++-1.0.2.tar.gz
-  tar zxf capnproto-c++-1.0.2.tar.gz
-  rm capnproto-c++-1.0.2.tar.gz
-  cd capnproto-c++-1.0.2
-  ./configure --prefix="$HOME/.local"
-  make -j6 check
-  make install
-  rm -rf capnproto-c++-1.0.2
-}
-
 # path setup
 setup_path() {
   if [ ! -f "$HOME/.bashrc" ]; then
@@ -31,12 +12,37 @@ setup_path() {
   source ~/.bashrc
 }
 
+# install capnp from source
+install_capnp() {
+  # don't install if already installed
+
+  if [ -f "$HOME/.local/bin/capnp" ]; then
+    echo "capnp already installed"
+    return
+  fi
+
+  echo # setting up path
+  setup_path
+
+  curl -O https://capnproto.org/capnproto-c++-1.0.2.tar.gz
+  tar zxf capnproto-c++-1.0.2.tar.gz
+  rm capnproto-c++-1.0.2.tar.gz
+  cd capnproto-c++-1.0.2
+  ./configure --prefix="$HOME/.local"
+  make -j6 check
+  make install
+  rm -rf capnproto-c++-1.0.2
+
+}
+
 # destroy default python environment and conda environments
 destroy_default_python_env() {
-  if [ $CONDA_SHLVL -gt 0 ]; then
-    for i in $(seq 1 $CONDA_SHLVL); do
-      conda deactivate
-    done 
+  if [ -n "$CONDA_SHLVL" ]; then
+    if [ "$CONDA_SHLVL" -gt 0 ]; then
+      for i in $(seq 1 $CONDA_SHLVL); do
+        conda deactivate
+      done 
+    fi
   fi
 
   pip freeze | xargs pip uninstall -y
@@ -66,9 +72,6 @@ rm -rf $HOME/.cache
 echo "Installing capnp from source..."
 install_capnp
 
-echo "Setting up path..."
-setup_path
-
 echo "Are you sure you want to destroy default python environment and conda environments? This action is irreversible"
 echo
 read -p "Continue? (y/n) " out
@@ -81,7 +84,14 @@ fi
 echo "destroying default python environment..."
 destroy_default_python_env
 
+
 echo "Creating new conda environment..."
 
-conda env create -f environment.yml
+conda env create -f ./scripts/environment.yml
 conda activate ml
+
+echo "installing additional dependencies"
+pip install zmq
+pip install gymnasium[classic-control]
+pip install moviepy
+pip install pymango
