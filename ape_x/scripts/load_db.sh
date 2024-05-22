9d2360b4ca6def71fe6c09d4feae8f5ffab1a741
Ezra Huang <ezrahuang155@gmail.com>
2024-05-22 13:11:16 -0400

    cleaner saving and loading scripts

--- ape_x/scripts/load_db.sh
+++ ape_x/scripts/load_db.sh
@@ -4,11 +4,9 @@ host=$1
 port=$2
 user=$3
 password=$4
-dir=$3
+location=$5
 
 uri="mongodb://$host:$port"
 db="model_weights"
 
-mongorestore --uri="$uri" --db="$db" -user="$user" --password="$password"--collection="fs.files" --out="$dir/gridFS/fs.files.bson "
-mongorestore --uri="$uri" --db="$db" --user="$user" --password="$password"--collection="fs.chunks" --out="$dir/gridFS/fs.chunks.bson "
-mongorestore --uri="$uri" --db="$db" --user="$user" --password="$password"--collection="ids" --out="$dir/ids.bson"
+mongorestore --uri="$uri" --db="$db" -user="$user" --password="$password" --archive="$location" --gzip
--- ape_x/scripts/save_db.sh
+++ ape_x/scripts/save_db.sh
@@ -4,11 +4,9 @@ host=$1
 port=$2
 user=$3
 password=$4
-dir=$3
+location=$5
 
 uri="mongodb://$host:$port"
 db="model_weights"
 
-mongoexport --uri="$uri" --db="$db" --user="$user" --password="$password" --collection="fs.files" --out="$dir"
-mongoexport --uri="$uri" --db="$db" --user="$user" --password="$password" --collection="fs.chunks" --out="$dir"
-mongoexport --uri="$uri" --db="$db" --user="$user" --password="$password" --collection="ids" --out="$dir"
+mongoexport --uri="$uri" --db="$db" --user="$user" --password="$password" --archive="$location" --gzip
