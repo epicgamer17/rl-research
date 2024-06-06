fe2cd1b04f13dff34774a1503578ec1ea5b279ef
Ezra Huang <ezrahuang155@gmail.com>
2024-06-05 22:36:46 -0600

    update setup scripts

--- dqn/ape_x/scripts/setup.sh
+++ dqn/ape_x/scripts/setup.sh
@@ -12,29 +12,6 @@ setup_path() {
   source ~/.bashrc
 }
 
-# install capnp from source
-install_capnp() {
-  # don't install if already installed
-
-  if [ -f "$HOME/.local/bin/capnp" ]; then
-    echo "capnp already installed"
-    return
-  fi
-
-  echo # setting up path
-  setup_path
-
-  curl -O https://capnproto.org/capnproto-c++-1.0.2.tar.gz
-  tar zxf capnproto-c++-1.0.2.tar.gz
-  rm capnproto-c++-1.0.2.tar.gz
-  cd capnproto-c++-1.0.2
-  ./configure --prefix="$HOME/.local"
-  make -j6 check
-  make install
-  rm -rf capnproto-c++-1.0.2
-
-}
-
 # destroy default python environment and conda environments
 destroy_default_python_env() {
   if [ -n "$CONDA_SHLVL" ]; then
@@ -53,7 +30,7 @@ destroy_default_python_env() {
   done
 }
 
-echo "This script will delete your ~/.cache directory, install capnp from source, completely wipe out your default python installation and conda installations to make disk space and setup a new conda environment for ape-x."
+echo "This script will delete your ~/.cache directory, completely wipe out your default python installation and conda installations to make disk space and setup a new conda environment for ape-x."
 echo
 echo "It is also recommended to run this script on one of the open-gpu-x servers as they are much faster than mimi."
 echo
@@ -68,9 +45,6 @@ fi
 
 echo "Deleting ~/.cache to make space..."
 rm -rf $HOME/.cache
-
-echo "Installing capnp from source..."
-install_capnp
 
 echo "Are you sure you want to destroy default python environment and conda environments? This action is irreversible"
 echo
