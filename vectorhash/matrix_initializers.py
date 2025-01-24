38c3ca673ebb5a6fc9704964476d686ecaceff20
Ezra Huang <ezrahuang155@gmail.com>
2025-01-24 17:01:01 -0500

    move matrix initializers to new file

---
+++ vectorhash/matrix_initializers.py
@@ -0,0 +1,33 @@
+import torch
+
+class SparseMatrixInitializer:
+    def __init__(self, device=None):
+        self.device = device
+        pass
+
+    def __call__(self, *args, **kwds):
+        pass
+
+
+class SparseMatrixBySparsityInitializer(SparseMatrixInitializer):
+    def __init__(self, sparsity, device=None):
+        super().__init__(device=device)
+        self.sparsity = sparsity
+
+    def __call__(self, shape):
+        return (
+            torch.normal(0, 1, shape, device=self.device)
+            * (torch.rand(shape, device=self.device) < self.sparsity).float()
+        )
+
+
+class SparseMatrixByScalingInitializer(SparseMatrixInitializer):
+    def __init__(self, scale, mean=0, std=1, device=None):
+        super().__init__(device=device)
+        self.device = device
+        self.scale = scale
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, shape):
+        return torch.normal(self.mean, self.std, shape, device=self.device) * self.scale
