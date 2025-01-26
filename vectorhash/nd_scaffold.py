4aa3fe90af85c5be5f5a8f70e3bf1aafb1e14c33
Ezra Huang <ezrahuang155@gmail.com>
2025-01-25 21:27:18 -0500

    improve documentation for grid scaffold

--- vectorhash/nd_scaffold.py
+++ vectorhash/nd_scaffold.py
@@ -1,22 +1,28 @@
 import torch
 import numpy as np
 from matrix_initializers import SparseMatrixBySparsityInitializer
-from vectorhash_functions import int_repr_onehot
 
 
 class GridModule:
     def __init__(self, shape: tuple[int], device=None, T=1) -> None:
+        """Initializes a grid module with a given shape and a temperature.
+
+        Args:
+            shape (tuple[int]): The shape of the grid.
+            T (int, optional): The temperature of the grid used with softmax when computing the onehot encoded state probabilities. Defaults to 1.
+        """
         self.shape = shape
         # self.state = torch.rand(shape, device=device)
         self.state = torch.zeros(shape, device=device)
-        self.state[0, 0, 0] = 1
+
+        i = tuple(0 for _ in range(len(shape)))
+        self.state[i] = 1
         self.l = torch.prod(torch.tensor(shape)).item()
+        """The number of elements in the grid, i.e. the product of each element in the shape. Ex. a `3x3x4` grid has `l=36`."""
         self.T = T
         self.device = device
 
     def denoise_onehot(self, onehot: torch.Tensor):
-        if onehot.ndim == 1:
-            onehot = onehot.unsqueeze(0)
         """Denoise a batch of one-hot encoded states.
 
         Input shape: `(B, l)` where l is the product of the shape of the grid.
@@ -26,11 +32,24 @@ class GridModule:
 
         Output shape: `(B, l)`
         """
+        if onehot.ndim == 1:
+            onehot = onehot.unsqueeze(0)
 
         state = onehot.view((onehot.shape[0], *self.shape))
         return self.denoise(state).flatten(1)
 
     def denoise(self, state: torch.Tensor):
+        """Denoise a batch of grid states. This finds the maximum value in the grid and sets it to 1, and all other values to 0.
+        If there are multiple maximum values, they are all set to 1 / number of maximum values.
+
+        Input shape: `(B, *shape)` where `shape` is the shape of the grid.
+
+        Args:
+            state: The tensor of grid states.
+
+        Output shape: `(B, *shape)` where `shape` is the shape of the grid.
+
+        """
         if state.ndim == len(self.shape):
             state = state.unsqueeze(0)
 
@@ -40,15 +59,17 @@ class GridModule:
             state == maxes, torch.ones_like(state), torch.zeros_like(state)
         )
         scaled = state / torch.sum(state, dim=dims, keepdim=True)
-        assert torch.allclose(
-            torch.sum(scaled, dim=dims), torch.ones(state.shape[0], device=state.device)
-        )
         return scaled
 
     def denoise_self(self):
+        """Denoises this grid module's state"""
         self.state = self.denoise(self.state).squeeze(0)
 
     def onehot(self):
+        """Returns the one-hot encoding of the state of this grid module.
+
+        Output shape: `(l)` where `l` is the product of the shape of the grid (i.e. a 3x3x4 grid has l=36).
+        """
         pdfs = list()
         dims = range(len(self.shape))
         for i in range(len(self.shape)):
@@ -64,6 +85,13 @@ class GridModule:
         return (torch.einsum(einsum_str, *pdfs).flatten() / self.T).softmax(dim=0)
 
     def shift(self, v):
+        """Shifts the state of the grid module by a given velocity.
+
+        Input shape: `(len(shape))`
+
+        Args:
+            v: The velocity by which to shift the grid state.
+        """
         self.state = torch.roll(
             self.state, (1, 0, 1), dims=tuple(i for i in range(len(self.shape)))
         )
@@ -90,6 +118,7 @@ class GridScaffold:
             return
 
         self.modules = [GridModule(shape, device=device, T=T) for shape in shapes]
+        """The list of grid modules in the scaffold."""
         self.N_g = sum([module.l for module in self.modules])
         self.N_patts = np.prod([module.l for module in self.modules]).item()
         self.N_h = N_h
@@ -102,6 +131,7 @@ class GridScaffold:
         self.sparse_matrix_initializer = sparse_matrix_initializer
 
         self.G = self._G()
+        """The matrix of all possible grid states. Shape: `(N_patts, N_g)`"""
 
         if sparse_matrix_initializer is None:
             sparse_matrix_initializer = SparseMatrixBySparsityInitializer(
@@ -109,14 +139,21 @@ class GridScaffold:
             )
 
         self.W_hg = sparse_matrix_initializer((self.N_h, self.N_g))
+        """The matrix of weights to go from the grid layer to the hippocampal layer. Shape: `(N_h, N_g)`"""
+
         self.H = self.hippocampal_from_grid(self.G)  # (N_patts, N_h)
+        """The matrix of all possible hippocampal states induced by `G` and `W_hg`. Shape: `(N_patts, N_h)`"""
+
         self.W_gh = self._W_gh()  # (N_g, N_h)
         self.W_sh = torch.zeros((self.input_size, self.N_h), device=device)
         self.W_hs = torch.zeros((self.N_h, self.input_size), device=device)
+
+        """The current grid coding state tensor. Shape: `(N_g)`"""
         self.g = self._g()
 
     @torch.no_grad()
     def _G(self):
+        """Calculates the matrix of all possible grid states. Shape: `(N_patts, N_g)`"""
         G = torch.zeros((self.N_patts, self.N_g), device=self.device)
         height = 0
         for module in self.modules:
@@ -128,6 +165,7 @@ class GridScaffold:
         return G
 
     def _g(self):
+        """Calculates the current grid coding state tensor. Shape: `(N_g)`"""
         vecs = list()
         for module in self.modules:
             vecs.append(module.onehot())
@@ -179,6 +217,7 @@ class GridScaffold:
 
     @torch.no_grad()
     def _W_gh(self):
+        """Calculates the matrix of weights to go from the hippocampal layer to the grid layer heteroassociatively. Shape: `(N_g, N_h)`"""
         return (
             torch.einsum("bi,bj->bij", self.G, self.H).sum(dim=0, keepdim=False)
             / self.N_h
@@ -255,31 +294,31 @@ class GridScaffold:
         return ret
 
     @torch.no_grad()
-    def store_memory(self, s: torch.Tensor, num_iterations=1):
-        """
-        Stores sensory input s into the memory model by learning weights.
-
-        :param s: Sensory input vector. - (input_size)
-        :param num_iterations: Number of iterations for updating the weights.
-        :return: None
+    def store_memory(self, s: torch.Tensor):
+        """Stores a memory in the scaffold.
+        Input shape: `(input_size)`
         """
         # https://github.com/tmir00/TemporalNeuroAI/blob/c37e4d57d0d2d76e949a5f31735f902f4fd2c3c7/model/model.py#L55C1-L55C69
-        for _ in range(num_iterations):
-            h = torch.relu(self.W_hg @ self.g - self.relu_theta)
+        h = torch.relu(self.W_hg @ self.g - self.relu_theta)
 
-            self.W_gh += self.calculate_update(input=h, output=self.g)
-            self.W_sh += self.calculate_update(input=h, output=s)
-            self.W_hs += self.calculate_update(input=s, output=h)
+        self.W_gh += self.calculate_update(input=h, output=self.g)
+        self.W_sh += self.calculate_update(input=h, output=s)
+        self.W_hs += self.calculate_update(input=s, output=h)
 
     @torch.no_grad()
     def shift(self, velocity):
+        """Shifts the grid coding state by a given velocity.
+
+        The length of `velocity` must be equal to the dimensionality of the grid modules.
+        """
         for module in self.modules:
             module.shift(velocity)
+
         self.g = self._g()
 
     @torch.no_grad()
     def denoise(self, G):
-        """
+        """Denoise a batch of grid coding states.
 
         Input shape: `(B, N_g)`
 
@@ -300,17 +339,34 @@ class GridScaffold:
 
     @torch.no_grad()
     def learn_path(self, observations, velocities):
+        """Add a path of observations to the memory scaffold. It is assumed that the observations are taken at each time step and the velocities are the velocities directly after the observations.
+        """
         for obs, vel in zip(observations, velocities):
             self.learn(obs, vel)
 
     @torch.no_grad()
     def learn(self, observation, velocity):
+        """Add a memory to the memory scaffold and shift the grid coding state by a given velocity.
+
+        observation shape: `(input_size)`
+        velocity shape: `(D)` where `D` is the dimensionality of the grid modules.
+        """
         self.store_memory(observation)
         self.shift(velocity)
-        pass
 
     @torch.no_grad()
     def recall(self, observations):
+        """Recall a (batch of) sensory input(s) from the scaffold.
+
+        Input shape: `(B, input_size)` where `B` is the batch size.
+        Output shape: `(B, input_size)` where `B` is the batch size.
+
+        Args:
+            observations (torch.Tensor): The tensor of batched sensory inputs to recall
+
+        Returns:
+            The tensor of batched sensory inputs recalled from the scaffold.
+        """
         # https://github.com/tmir00/TemporalNeuroAI/blob/c37e4d57d0d2d76e949a5f31735f902f4fd2c3c7/model/model.py#L96
         # noisy_observations: (N, input_size)
         H = self.hippocampal_from_sensory(observations)
