a8f20aa12f8b0679f2c8893d7ecb905d3146a56c
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2025-02-01 16:32:28 -0500

    velocity shift should be working

--- vectorhash/ratslam_velocity_shift.py
+++ vectorhash/ratslam_velocity_shift.py
@@ -1,25 +1,32 @@
 from calendar import c
+from hmac import new
 import math
 import numpy as np
 
 
 # grid cell to real world size scaling
-k_x = 1 / 2
-k_y = 1 / 2
+k_x = 1
+k_y = 1
 k_theta = 1
 
 
 # for every pose cell, calculate the velocity shift
 def inject_activity(pose_cells, v, theta, omega):
+    new_pose_cells = np.copy(pose_cells)
     for g_x in range(pose_cells.shape[0]):
         for g_y in range(pose_cells.shape[1]):
             for g_theta in range(pose_cells.shape[2]):
-                pose_cells[g_x][g_y][g_theta] = pose_cells[g_x][g_y][
-                    g_theta
-                ] + calculate_velocity_shift(
+                # new_pose_cells[g_x][g_y][g_theta] = pose_cells[g_x][g_y][
+                #     g_theta
+                # ] + calculate_velocity_shift(
+                #     pose_cells, g_x, g_y, g_theta, v, theta, omega
+                # )
+                # not keeping old beliefs
+                new_pose_cells[g_x][g_y][g_theta] = calculate_velocity_shift(
                     pose_cells, g_x, g_y, g_theta, v, theta, omega
                 )
-    return pose_cells
+
+    return new_pose_cells
 
 
 def calculate_velocity_shift(pose_cells, l, m, n, v, theta, omega):
@@ -27,17 +34,33 @@ def calculate_velocity_shift(pose_cells, l, m, n, v, theta, omega):
     delta_x, delta_y, delta_theta = calculate_deltas(
         v, theta, omega
     )  # speed and angular velocity of the robot
-    print(delta_x, delta_y, delta_theta)
     delta_f_x, delta_f_y, delta_f_theta = calculate_delta_fs(
-        delta_x, delta_y, delta_theta, theta, omega
+        delta_x, delta_y, delta_theta, v, theta, omega
     )
-    alpha = calculate_alpha(
-        delta_x, delta_y, delta_theta, delta_f_x, delta_f_y, delta_f_theta
-    )
-    for x in range(delta_x, delta_x + 1):
-        for y in range(delta_y, delta_y + 1):
-            for theta in range(delta_theta, delta_theta + 1):
-                change += alpha[x][y][theta] * pose_cells[l + x][m + y][n + theta]
+    alpha = calculate_alpha(delta_f_x, delta_f_y, delta_f_theta)
+    print("deltas", delta_x, delta_y, delta_theta)
+    for x in range(delta_x, delta_x + 2):
+        for y in range(delta_y, delta_y + 2):
+            for theta in range(delta_theta, delta_theta + 2):
+                change += (
+                    alpha[x - delta_x][y - delta_y][
+                        theta - delta_theta
+                    ]  # paper does x y theta (but there alpha indices are weird)
+                    * pose_cells[min(l + x, len(pose_cells) - 1)][
+                        min(m + y, len(pose_cells[0]) - 1)
+                    ][min(n + theta, len(pose_cells[0][0]) - 1)]
+                )
+                print(
+                    "alpha cell value",
+                    alpha[x - delta_x][y - delta_y][theta - delta_theta],
+                )
+                print(
+                    "observed cell",
+                    pose_cells[min(l + x, len(pose_cells) - 1)][
+                        min(m + y, len(pose_cells[0]) - 1)
+                    ][min(n + theta, len(pose_cells[0][0]) - 1)],
+                )
+                print("intermediate change", change)
     print("change", change)
     return change
 
@@ -52,29 +75,35 @@ def calculate_deltas(
     return delta_x, delta_y, delta_theta
 
 
-def calculate_delta_fs(delta_x, delta_y, delta_theta, theta, omega):
-    delta_f_x = k_x * np.cos(theta) - delta_x
-    delta_f_y = k_y * np.sin(theta) - delta_y
+def calculate_delta_fs(delta_x, delta_y, delta_theta, v, theta, omega):
+    delta_f_x = (
+        k_x * v * np.cos(theta) - delta_x
+    )  # i think they forgot this in the paper equations
+    delta_f_y = k_y * v * np.sin(theta) - delta_y
     delta_f_theta = k_theta * omega - delta_theta
 
     return delta_f_x, delta_f_y, delta_f_theta
 
 
-def calculate_alpha(delta_x, delta_y, delta_theta, delta_f_x, delta_f_y, delta_f_theta):
+def calculate_alpha(
+    delta_f_x, delta_f_y, delta_f_theta
+):  # delta_x, delta_y, delta_theta
     alpha = np.zeros((2, 2, 2))
     for i in range(0, 2):
         for j in range(0, 2):
             for k in range(0, 2):
                 alpha[i][j][k] = (
-                    g(delta_f_x, i - delta_x)
-                    * g(delta_f_y, j - delta_y)
-                    * g(delta_f_theta, k - delta_theta)
+                    g(
+                        delta_f_x, i
+                    )  # paper has i - delta_x, but its really just saying floored value gets 1 - a, and the other gets a
+                    * g(delta_f_y, j)
+                    * g(delta_f_theta, k)
                 )
     print("alpha", alpha)
     return alpha
 
 
-# this seemingly has some errors in the paper as b can be values other than 0 or 1
+# this seemingly has some errors in the paper as b can be values other than 0 or 1, but we can have any (integer) value for b (maybe we need to tune k? or is it mod?)
 def g(a, b):
     if b == 0:
         return 1 - a
