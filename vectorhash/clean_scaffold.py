3347241c136f96df6615d8c9ecaf7121ceae93ff
Ezra Huang <ezrahuang155@gmail.com>
2025-03-31 11:33:29 -0400

    fix indexing bug in estimate_certainty

--- vectorhash/clean_scaffold.py
+++ vectorhash/clean_scaffold.py
@@ -307,7 +307,9 @@ class GridHippocampalScaffold:
         for dim in range(len(self.shapes[0])):
             marginals = [module.get_marginal(dim) for module in self.modules]
             v = expand_distribution(marginals)
-            mean = circular_mean(v * torch.arange(0, len(v)), len(v)) % len(v)
+            mean = circular_mean(v * torch.arange(0, len(v)), len(v))
+            if mean > len(v) // 2:
+                mean -= len(v)
             low = torch.ceil(mean - k)
             high = torch.floor(mean + k)
             indices = torch.arange(low, high + 1).int()
